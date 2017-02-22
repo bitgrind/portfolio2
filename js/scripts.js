b05a33638a4b5c5108603f867647fbf692e874b6
bitgrind <keithstedman@gmail.com>
2017-02-22 06:52:08 -0800

    as far as I could get it with the time I had

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,14 @@ $(function(){
   function animateDown(x) {
     $(x).animate({height:'400px'},300);
     $(x).css('overflow', 'visible');
-    console.log(x.children('div'));
-    x.children('div').css('overflow', 'visible');
+    $('.projects').show(300);
+    $('.past-experience-logo').css('overflow', 'visible');
   }
   function animateUp(x) {
     $(x).animate({'height':'80px'},300);
     $(x).css('overflow', 'hidden');
-    x.children('div').css('overflow', 'hidden');
+    $('.past-experience-logo').css('overflow', 'hidden');
+    $('.projects').hide(200);
   }
   $('.past-experience').click(function(){
     height = $(this).css('height');
