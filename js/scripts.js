ec6dedacbc644523b47dc0fdeeb8cea242c7f850
bitgrind <keithstedman@gmail.com>
2017-02-22 06:11:59 -0800

    js interactivity working

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(function(){
 
   function animateDown(x) {
-    $(x).animate({'height':'400px'},300);
+    $(x).animate({height:'400px'},300);
+    $(x).css('overflow', 'visible');
+    console.log(x.children('div'));
+    x.children('div').css('overflow', 'visible');
   }
   function animateUp(x) {
     $(x).animate({'height':'80px'},300);
+    $(x).css('overflow', 'hidden');
+    x.children('div').css('overflow', 'hidden');
   }
   $('.past-experience').click(function(){
-    $('.past-experience').childern
+    height = $(this).css('height');
+    console.log(height);
+
     if(height == '80px'){
+      console.log('open down');
       animateDown($(this));
     }
     if(height == '400px') {
+      console.log('open up');
       animateUp($(this));
     }
   });
